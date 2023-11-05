f6fb2092273987e49b0622d593d1a7528e6ec72f
markyasser <82395903+markyasser@users.noreply.github.com>
2023-11-05 14:05:51 +0200

    Created Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatchReservationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
