827c23959d18060d2d4c9e32a87b65fe32a80d0a
markyasser <82395903+markyasser@users.noreply.github.com>
2023-11-26 22:34:23 +0200

    Delete User

---
+++ Controllers/API/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using MarkReservationSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarkReservationSystem.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Delete(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            var result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception("Failed to delete user");
+            }
+            return Ok();
+        }
+    }
+}
