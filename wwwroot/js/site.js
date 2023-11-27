6908713eb323689ae2a54fa8ae141843c706b164
Bemoi Erian <bemoi.erian@gmail.com>
2023-11-27 20:33:29 +0200

    added functionality for grid buttons

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+	// var seatPosition = $("#SeatPosition").val();
+	// $(".grid-btn").each(function () {
+	// 	if ($(this).text() == seatPosition) {
+	// 		$(this).addClass("active");
+	// 	}
+	// });
+	$(".grid-btn").on("click",function () {
+		$(".grid-btn").removeClass("grid-btn-selected");
+		if (!$(this).hasClass("grid-btn-rsrv")) {
+			$(this).addClass("grid-btn-selected");
+		}
+		console.log("Clicked");
+		$("#SeatPosInput").val($(this).text());
+	});
+});
